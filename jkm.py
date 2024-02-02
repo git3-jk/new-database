a607b63c7303c796e8de4679982a02770336145c
git3-jk <jasavant369@gmail.com>
2024-02-02 19:55:49 +0530

    this is a new file.

--- jkm.py
+++ jkm.py
@@ -1,9 +1,1 @@
 #print("Hello World")
-message="keep rocking"
-id=10
-print ('this is a test lets see if it is working',id)
-print (message)
-ids=3+4j
-print(ids)
-jk='new chief minister'
-print(jk)
